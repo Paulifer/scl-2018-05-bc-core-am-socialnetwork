d0c05eb3ab844d4381e5cbda1f0531eb9803ef71
Paulifer <pauli.hua.timm@gmail.com>
2018-07-09 00:29:40 -0400

    añadi la funcion para validar email

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,12 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
+//Iniciar seción a traves de correo
+const btnIngresarMail = document.getElementById('btnenviar').addEventListener('click', ()=>{
+	const validarMail = document.getElementById('exampleInputEmail1').value;
+	if (/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(validarMail)) {
+		alert("correcto");
+	}else{
+		alert("incorrecto");
+	}
+})
