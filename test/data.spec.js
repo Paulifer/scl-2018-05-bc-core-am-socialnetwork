91e0610d0023302a11ca5394265ba1bf023f1563
ConstanzaGarcia <o.conii.o@gmail.com>
2018-07-19 11:40:03 -0400

    test en desarrollo

---
+++ test/data.spec.js
@@ -0,0 +1,38 @@
+// Initialize Firebase
+
+var config = {
+    apiKey: "AIzaSyCg7hHQY9fRP9EJTE4R3-HPUMi4wFA0WaI",
+    authDomain: "redsocial-78423.firebaseapp.com",
+    databaseURL: "https://redsocial-78423.firebaseio.com",
+    projectId: "redsocial-78423",
+    storageBucket: "redsocial-78423.appspot.com",
+    messagingSenderId: "739042868398"
+};
+firebase.initializeApp(config);
+
+
+//test para ver si el login funciona 
+describe ('funciones que debe tener el login', () => {
+    it ('Debe dar verdadero para email y clave', () => {
+        const email = 'test@mail.com'
+        const password = 12345
+        //const result = window.controller.validateLogin(variales declaras, variables declaras);
+        //asert.equal valida resultado
+        //asert.aqual valida resultado
+    });
+
+    it 
+
+    
+})
+
+describe ('funciones que debe tener el muro', () => {
+    it ('Debe dar falso para la ausencia de texto', () => {
+        const
+        const
+        //const result = window.controller.validateMuro(variables declaradas, variables declaras);
+    });
+
+
+    it 
+})
