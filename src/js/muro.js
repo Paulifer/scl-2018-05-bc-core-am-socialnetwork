a9e4976d828e9d3466795d9b514d649f9cef4aa9
camila <camiramirezpp@gmail.com>
2018-07-19 22:00:25 -0400

    mensajes aparecen desde arriba

--- src/js/muro.js
+++ src/js/muro.js
@@ -14,19 +14,17 @@ firebase.database().ref('messages')
 firebase.database().ref('messages')
   .limitToLast(5) //muestra solo los ultimos 5 mensajes como historial al recargar la pagina
   .on('child_added', (newMessage) => {
-    contenedor.innerHTML += `
+    contenedor.innerHTML = `
               <div class="trashPost" style="margin: 7%; border-radius:8px; background-color:#ECF8E0">
                 <p style="margin-left:0.5em; color:#9B369D;">${newMessage.val().creatorName} ha comentado:</p>
                 <p style="margin-left:0.5em;">${newMessage.val().text}</p>
                 <i class="fa fa-heart heart iconHeart" onclick="like(event)" data-likePost="${newMessage.key}"><span> ${newMessage.val().starCount}</span></i>
-                <i class="fas fa-pencil-alt iconEdit"></i>
+                <i class="fas fa-pencil-alt iconEdit"  data-id="${newMessage.key}"></i>
                 <i class="fa fa-trash trash iconTrash" onclick="deletePost(event)" 
                 data-postId="${newMessage.key}"></i>
               </div>
-          `;
-  
+          `+ contenedor.innerHTML;
   });
-
 
 
 //Boton me gusta
@@ -38,7 +36,6 @@ const like = (event) => {
     let result = (posting.val().starCount || 0)+1;
     console.log(result);
     
-
     firebase.database().ref('messages').child(idLike).update({
       starCount : result
     });
@@ -52,7 +49,7 @@ const deletePost = (event) => {
   if(confirmar === true){
     const idPosts = event.target.getAttribute('data-postId');
     firebase.database().ref('messages/').child(idPosts).remove();
-    
+    // contenedor.removeChild(contenedor.childNodes[0] && contenedor.childNodes[1]); //se borra el primer mensaje
   }else{};
 };
 
